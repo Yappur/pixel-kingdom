68570de5ec038dc8044ebc5e620da043b366b934
unknown <mateox463@gmail.com>
2025-03-12 16:43:56 -0300

    se modifican estilos y se añaden useStates

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,21 +2,38 @@ import React, { useState } from "react";
 import "../styles/editor.css";
 
 const Editor = () => {
+  const [PanelWidht, setPanelWidht] = useState(16);
+  const [PanelHeight, setPanelHeight] = useState(16);
+  const [hideOptions, sethideOptions] = useState(false);
+  const [hideDrawingPanel, sethideDrawingPanel] = useState(true);
+  const [buttonText, setButtonText] = useState("start drawing");
+  const [selectedColor, setColor] = useState("#000000");
+
   return (
     <div className="editor">
       <h1>Pixel Editor</h1>
       <h2>Igresa las dimensiones de tu lienzo</h2>
       <div className="opcionesNum">
         <div className="opcionesInput">
-          <input type="number" className="numberInput" defaultValue={16} />
-          <input type="number" className="numberInput" defaultValue={16} />
-          <br />
+          <input
+            type="number"
+            className="numberInput"
+            defaultValue={PanelWidht}
+            onChange={(e) => setPanelWidht(e.target.value)}
+          />
           <span>Largo </span>
+          <br />
+          <input
+            type="number"
+            className="numberInput"
+            defaultValue={PanelHeight}
+            onChange={(e) => setPanelHeight(e.target.value)}
+          />
           <span> Ancho</span>
         </div>
       </div>
 
-      <button className="button">Crear</button>
+      <button className="button">{buttonText}</button>
     </div>
   );
 };
